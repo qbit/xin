466345a2eee16319a337cb440cebdbfa5ad59fc3
Aaron Bieber <aaron@bolddaemon.com>
2022-09-02 08:08:24 -0600

    overlays: remove wireplumber patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -51,18 +51,6 @@
 
       });
     })
-
-    (self: super: {
-      wireplumber = super.wireplumber.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [
-          (super.fetchpatch {
-            url =
-              "https://gitlab.freedesktop.org/pipewire/wireplumber/-/merge_requests/398.patch";
-            sha256 = "sha256-rEp/3fjBRbkFuw4rBW6h8O5hcy/oBP3DW7bPu5rVfNY=";
-          })
-        ];
-      });
-    })
   ] else
     [ ];
 }
