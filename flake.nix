f3efe161858ed04b22f94da28bdafe18b60312f4
Aaron Bieber <aaron@bolddaemon.com>
2022-09-02 12:46:13 -0600

    flake shell: add management key

--- flake.nix
+++ flake.nix
@@ -78,6 +78,7 @@
         pkgs.mkShell {
           shellHook = ''
             PS1='\u@\h:\w; '
+            ssh-add /run/secrets/manager_key
           '';
           nativeBuildInputs = with pkgs; [
             git
