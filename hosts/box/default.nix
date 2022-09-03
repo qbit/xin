87afacfd241a9ee734176ddc9fe3205c348bfaef
Aaron Bieber <aaron@bolddaemon.com>
2022-09-03 14:25:47 -0600

    box: setup promtail and add it to grafana

--- hosts/box/default.nix
+++ hosts/box/default.nix
@@ -287,6 +287,29 @@ in {
       domain = "graph.tapenet.org";
       port = 2342;
       addr = "127.0.0.1";
+      analytics.reporting.enable = false;
+
+      provision = {
+        enable = true;
+        datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            access = "proxy";
+            url =
+              "http://127.0.0.1:${toString config.services.prometheus.port}";
+          }
+          {
+            name = "Loki";
+            type = "loki";
+            access = "proxy";
+            url = "http://127.0.0.1:${
+                toString
+                config.services.loki.configuration.server.http_listen_port
+              }";
+          }
+        ];
+      };
     };
 
     loki = {
@@ -351,6 +374,37 @@ in {
           shared_store = "filesystem";
           compactor_ring = { kvstore = { store = "inmemory"; }; };
         };
+      };
+    };
+
+    promtail = {
+      enable = true;
+      configuration = {
+        server = {
+          http_listen_port = 3031;
+          grpc_listen_port = 0;
+        };
+        positions = { filename = "/tmp/positions.yaml"; };
+        clients = [{
+          url = "http://127.0.0.1:${
+              toString
+              config.services.loki.configuration.server.http_listen_port
+            }/loki/api/v1/push";
+        }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "box";
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }];
       };
     };
 
