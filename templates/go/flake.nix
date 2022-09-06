c842de84397719484c53b9939f0356c51da5a742
Aaron Bieber <aaron@bolddaemon.com>
2022-09-06 17:59:05 -0600

    use an easier to set template

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -5,9 +5,6 @@
 
   outputs = { self, nixpkgs }:
     let
-      lastModifiedDate =
-        self.lastModifiedDate or self.lastModified or "19700101";
-      version = builtins.substring 0 8 lastModifiedDate;
       supportedSystems =
         [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -18,7 +15,7 @@
         in {
           thing = pkgs.buildGoModule {
             pname = "thing";
-            inherit version;
+            version = "v0.0.0";
             src = ./.;
 
             vendorSha256 = pkgs.lib.fakeSha256;
@@ -33,6 +30,7 @@
           default = pkgs.mkShell {
             shellHook = ''
               PS1='\u@\h:\@; '
+              echo "Go `${pkgs.go}/bin/go version`"
             '';
             nativeBuildInputs = with pkgs; [ git go gopls go-tools ];
           };
