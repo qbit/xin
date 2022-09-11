7716b26731ca4511b3ce927052d9db991e04c0ce
Aaron Bieber <aaron@bolddaemon.com>
2022-09-10 21:51:29 -0600

    h: enable promtail

--- hosts/h/default.nix
+++ hosts/h/default.nix
@@ -131,6 +131,33 @@ in {
   };
 
   services = {
+    promtail = {
+      enable = true;
+      configuration = {
+        server = {
+          http_listen_port = 3031;
+          grpc_listen_port = 0;
+        };
+        positions = { filename = "/tmp/positions.yaml"; };
+        clients = [{
+          url = "http://box.humpback-trout.ts.net:3030/loki/api/v1/push";
+        }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "box";
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }];
+      };
+    };
     prometheus = {
       enable = true;
       port = 9001;
