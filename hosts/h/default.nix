5fbe5464b707545b3a25122e608a964b72debfe9
Aaron Bieber <aaron@bolddaemon.com>
2022-09-14 06:24:56 -0600

    h: enable gts again

--- hosts/h/default.nix
+++ hosts/h/default.nix
@@ -132,7 +132,7 @@ in {
 
   services = {
     gotosocial = {
-      enable = false;
+      enable = true;
       # https://github.com/superseriousbusiness/gotosocial/blob/v0.5.0-rc1/example/config.yaml
       configuration = {
         account-domain = "mammothcircus.com";
