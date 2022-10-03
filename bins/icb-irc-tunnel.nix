1c5bf23271dbc859b12275ab9edeb8e87cc99f06
Aaron Bieber <aaron@bolddaemon.com>
2022-10-03 09:32:33 -0600

    h: fire off tmux and weechat in the bg

--- bins/icb-irc-tunnel.nix
+++ bins/icb-irc-tunnel.nix
@@ -6,6 +6,13 @@
 
   ${icbirc}/bin/icbirc -l 127.0.0.1 -s localhost -p 6644
   ${icbirc}/bin/icbirc -l 127.0.0.1 -s localhost -p 6645
+
+  tname="IRC"
+  if !tmux ls | grep -q "^${tname}:"; then
+    tmux -2 new-session -d -s "${tname}" 'weechat'
+    tmux -s "${tname}" splitw -dv -b -h -l 30% 'ssh anonicb@slackers.openbsd.org'
+  fi
+
   ${pkgs.openssh}/bin/ssh -NTL 7326:localhost:7326 \
     -oServerAliveInterval=60 \
     -oExitOnForwardFailure=yes \
