9695dd5111ba6136f2297ebb3127bf3bd1688598
Aaron Bieber <aaron@bolddaemon.com>
2022-10-05 07:51:18 -0600

    overlay: temp fix for #194589

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,35 +1,54 @@
 { self, config, pkgs, lib, isUnstable, ... }:
 
 {
-  nixpkgs.overlays = if isUnstable then
-    [
+  nixpkgs.overlays = if isUnstable then [
 
-      (self: super: {
-        zig = super.zig.overrideAttrs (old: {
-          version = "0.10.0-dev.35e0ff7";
-          src = super.fetchFromGitHub {
-            owner = "ziglang";
-            repo = "zig";
-            rev = "10e11b60e56941cb664648dcebfd4db3d2efed30";
-            hash = "sha256-oD5yfvaaVtgW/VE+5yHCiJgC+QMwiLe2i+PGX3g/PT0=";
-          };
+    # https://github.com/NixOS/nixpkgs/pull/194589
+    (self: super: {
+      tidal-hifi = super.tidal-hifi.overrideAttrs (old: {
+        postFixup = ''
+          makeWrapper $out/opt/tidal-hifi/tidal-hifi $out/bin/tidal-hifi \
+            --prefix LD_LIBRARY_PATH : "${
+              lib.makeLibraryPath super.tidal-hifi.buildInputs
+            }" \
+            "''${gappsWrapperArgs[@]}"
+          substituteInPlace $out/share/applications/tidal-hifi.desktop \
+            --replace "/opt/tidal-hifi/tidal-hifi" "tidal-hifi" \
+            --replace "/usr/share/icons/hicolor/0x0/apps/tidal-hifi.png" "tidal-hifi.png"
+          for size in 48 64 128 256 512; do
+            mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps/
+            convert $out/share/icons/hicolor/0x0/apps/tidal-hifi.png \
+              -resize ''${size}x''${size} $out/share/icons/hicolor/''${size}x''${size}/apps/tidal-hifi.png
+          done
+        '';
+      });
+    })
 
-          patches = [ ];
+    (self: super: {
+      zig = super.zig.overrideAttrs (old: {
+        version = "0.10.0-dev.35e0ff7";
+        src = super.fetchFromGitHub {
+          owner = "ziglang";
+          repo = "zig";
+          rev = "10e11b60e56941cb664648dcebfd4db3d2efed30";
+          hash = "sha256-oD5yfvaaVtgW/VE+5yHCiJgC+QMwiLe2i+PGX3g/PT0=";
+        };
 
-          nativeBuildInputs = with pkgs; [ cmake llvmPackages_14.llvm.dev ];
+        patches = [ ];
 
-          buildInputs = with pkgs;
-            [ libxml2 zlib ] ++ (with llvmPackages_14; [ libclang lld llvm ]);
+        nativeBuildInputs = with pkgs; [ cmake llvmPackages_14.llvm.dev ];
 
-          checkPhase = ''
-            runHook preCheck
-            runHook postCheck
-          '';
+        buildInputs = with pkgs;
+          [ libxml2 zlib ] ++ (with llvmPackages_14; [ libclang lld llvm ]);
 
-        });
-      })
-    ]
-  else
+        checkPhase = ''
+          runHook preCheck
+          runHook postCheck
+        '';
+
+      });
+    })
+  ] else
     [ ];
 }
 
