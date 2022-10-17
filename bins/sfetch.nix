b9b9289d9fce26696c973f2dba1316888318d6ab
Aaron Bieber <aaron@bolddaemon.com>
2022-10-17 11:12:01 -0600

    fmt

--- bins/sfetch.nix
+++ bins/sfetch.nix
@@ -1,18 +1,18 @@
 { curl, gosignify }:
 
 ''
-    #!/usr/bin/env sh
+  #!/usr/bin/env sh
 
-    set -e
+  set -e
 
-    SERVER=cdn.openbsd.org
-    ITEM=$1
-    MACHINE=amd64
-    V=$(echo $ITEM | sed 's/[^0-9]//g')
-    [[ ! -z $2 ]] && MACHINE=$2
-    ${curl}/bin/curl -s -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
-    ${curl}/bin/curl -s -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
+  SERVER=cdn.openbsd.org
+  ITEM=$1
+  MACHINE=amd64
+  V=$(echo $ITEM | sed 's/[^0-9]//g')
+  [[ ! -z $2 ]] && MACHINE=$2
+  ${curl}/bin/curl -s -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
+  ${curl}/bin/curl -s -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
 
-    ${gosignify}/bin/gosignify -C -p "/etc/signify/openbsd-$V-base.pub" -x SHA256.sig "$ITEM"
+  ${gosignify}/bin/gosignify -C -p "/etc/signify/openbsd-$V-base.pub" -x SHA256.sig "$ITEM"
 
 ''
