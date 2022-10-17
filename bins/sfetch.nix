745eccd3ca49f8673f5486415fbde775ec2cc4cc
Aaron Bieber <aaron@bolddaemon.com>
2022-10-17 10:02:22 -0600

    bins/sfetch: shhhh

--- bins/sfetch.nix
+++ bins/sfetch.nix
@@ -10,8 +10,8 @@
     MACHINE=amd64
     V=$(echo $ITEM | sed 's/[^0-9]//g')
     [[ ! -z $2 ]] && MACHINE=$2
-    ${curl}/bin/curl -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
-    ${curl}/bin/curl -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
+    ${curl}/bin/curl -s -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
+    ${curl}/bin/curl -s -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
 
     ${gosignify}/bin/gosignify -C -p "/etc/signify/openbsd-$V-base.pub" -x SHA256.sig "$ITEM"
 
