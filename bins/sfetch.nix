02e4a507359389853660ac36f07b6624d6577cac
Aaron Bieber <aaron@bolddaemon.com>
2022-10-17 09:59:32 -0600

    pkgs: fix sfetch

--- bins/sfetch.nix
+++ bins/sfetch.nix
@@ -1,4 +1,4 @@
-{ minisign, curl }:
+{ curl, gosignify }:
 
 ''
     #!/usr/bin/env sh
@@ -8,16 +8,11 @@
     SERVER=cdn.openbsd.org
     ITEM=$1
     MACHINE=amd64
-    VER=snapshots
-    V=7.1
+    V=$(echo $ITEM | sed 's/[^0-9]//g')
     [[ ! -z $2 ]] && MACHINE=$2
-    if [[ ! -z $3 ]]; then
-  	VER=$3
-  	V=$(echo $VER | sed 's/\.//')
-    fi
-    ${curl}/bin/curl -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/$VER/$MACHINE/$ITEM" && \
-    ${curl}/bin/curl -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/$VER/$MACHINE/SHA256.sig"
+    ${curl}/bin/curl -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
+    ${curl}/bin/curl -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
 
-    ${minisign}/bin/minisign -C -p "/etc/signify/openbsd-$V-base.pub" -x SHA256.sig "$ITEM"
+    ${gosignify}/bin/gosignify -C -p "/etc/signify/openbsd-$V-base.pub" -x SHA256.sig "$ITEM"
 
 ''
