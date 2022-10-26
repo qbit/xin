8481241a390cce2a543651bc897c88ca68ef8da5
Aaron Bieber <aaron@bolddaemon.com>
2022-10-26 16:16:34 -0600

    bins: add xin-status for single shot status over ssh

---
+++ bins/xin-status.nix
@@ -0,0 +1,16 @@
+{ perl, perlPackages, ... }:
+
+''
+  #!${perl}/bin/perl
+
+  use strict;
+  use warnings;
+  use Data::Dumper;
+
+  use lib "${perlPackages.JSON}/${perl.libPrefix}/${perl.version}/";
+  use JSON qw{ decode_json encode_json };
+
+  my $info = decode_json(`nixos-version --json`);
+  $info->{needs_restart} = system('check-restart >/dev/null') == 0 ? JSON::false : JSON::true;
+  print encode_json $info;
+''
