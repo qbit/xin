6fc4b63b6d8aab632d5e8acb3280a40067eda2dd
Aaron Bieber <aaron@bolddaemon.com>
2022-11-01 11:04:15 -0600

    overlay: remove unstable overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -30,34 +30,9 @@ let
     });
   };
 in {
-  nixpkgs.overlays = if isUnstable then [
-    openssl_3
-
-    (self: super: {
-      zig = super.zig.overrideAttrs (old: {
-        version = "0.10.0-dev.35e0ff7";
-        src = super.fetchFromGitHub {
-          owner = "ziglang";
-          repo = "zig";
-          rev = "10e11b60e56941cb664648dcebfd4db3d2efed30";
-          hash = "sha256-oD5yfvaaVtgW/VE+5yHCiJgC+QMwiLe2i+PGX3g/PT0=";
-        };
-
-        patches = [ ];
-
-        nativeBuildInputs = with pkgs; [ cmake llvmPackages_14.llvm.dev ];
-
-        buildInputs = with pkgs;
-          [ libxml2 zlib ] ++ (with llvmPackages_14; [ libclang lld llvm ]);
-
-        checkPhase = ''
-          runHook preCheck
-          runHook postCheck
-        '';
-
-      });
-    })
-  ] else [
+  nixpkgs.overlays = if isUnstable then
+    [ ]
+  else [
     openssh
     openssl_3
 
