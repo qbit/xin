34cb73e86305df613e2a62043ba22b44f495309e
Aaron Bieber <aaron@bolddaemon.com>
2022-11-03 12:22:47 -0600

    bins/sfetch: fix handling of bsd files

--- bins/sfetch.nix
+++ bins/sfetch.nix
@@ -7,8 +7,8 @@
 
   SERVER=cdn.openbsd.org
   ITEM=$1
-  MACHINE=amd64
-  V=$(echo $ITEM | sed 's/[^0-9]//g')
+  MACHINE=''${2:-amd64}
+  V="72"
   [[ ! -z $2 ]] && MACHINE=$2
   ${curl}/bin/curl -s -o "$PWD/$ITEM" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/$ITEM" && \
   ${curl}/bin/curl -s -o "$PWD/SHA256.sig" "https://$SERVER/pub/OpenBSD/snapshots/$MACHINE/SHA256.sig"
