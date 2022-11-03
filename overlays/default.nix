1e71311f289706b3b5058b02d1438ab6006abf73
Aaron Bieber <aaron@bolddaemon.com>
2022-11-02 20:19:15 -0600

    overlay: add example of overlaying buildGoModule

--- overlays/default.nix
+++ overlays/default.nix
@@ -68,3 +68,19 @@ in {
 #   };
 # })
 
+# Example of an overlay that changes the buildGoModule function
+#tailscale = self: super: {
+#  tailscale = super.callPackage "${super.path}/pkgs/servers/tailscale" {
+#    buildGoModule = args:
+#      super.buildGo119Module (args // rec {
+#        version = "1.32.2";
+#        src = super.fetchFromGitHub {
+#          owner = "tailscale";
+#          repo = "tailscale";
+#          rev = "v${version}";
+#          sha256 = "sha256-CYNHD6TS9KTRftzSn9vAH4QlinqNgU/yZuUYxSvsl/M=";
+#        };
+#        vendorSha256 = "sha256-VW6FvbgLcokVGunTCHUXKuH5+O6T55hGIP2g5kFfBsE=";
+#      });
+#  };
+#};
