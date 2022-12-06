de819ee251e194295ed5c34c61543a613a6aeaee
Aaron Bieber <aaron@bolddaemon.com>
2022-12-06 13:34:06 -0700

    pkgs/gotosocial: bump to 0.6.0 release

--- pkgs/gotosocial.nix
+++ pkgs/gotosocial.nix
@@ -1,11 +1,11 @@
 { stdenv, lib, buildGo119Module, fetchFromGitHub, makeWrapper, go, git, ffmpeg
 , ... }:
 let
-  gotosocialVersion = "0.6.0-rc3";
+  gotosocialVersion = "0.6.0";
   gotosocialWebAssets = builtins.fetchurl {
     url =
       "https://github.com/superseriousbusiness/gotosocial/releases/download/v${gotosocialVersion}/gotosocial_${gotosocialVersion}_web-assets.tar.gz";
-    sha256 = "sha256:1x4ylhmm2rnhif9rpxnjgpml1c4gasi4c5bdzhnqgh7cw24ssnrh";
+    sha256 = "sha256:17sfny68wx3a16sl1fkfxvr7225nyaf0d4r4gw1phis65l7cj1s4";
   };
 in with lib;
 buildGo119Module rec {
@@ -16,7 +16,7 @@ buildGo119Module rec {
     owner = "superseriousbusiness";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-OOiHfWUaBIHsxOAcnzc6GwepvhMbvdX63GWVXHX5FwY=";
+    hash = "sha256-VL0B+8n5mmr1qd5hSdURmaNxqnSaUynGu1qsXo/D+dQ=";
   };
 
   ldflags = [
