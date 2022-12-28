a4cde7b8610617f6353acb64c892b947c306820b
Aaron Bieber <aaron@bolddaemon.com>
2022-12-28 14:42:00 -0700

    fmt

--- gui/xfce.nix
+++ gui/xfce.nix
@@ -1,8 +1,22 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib; {
   options = { xfce = { enable = mkEnableOption "Enable XFCE desktop."; }; };
 
   config = mkIf config.xfce.enable {
+    security.pam.services = {
+      gdm.enableKwallet = true;
+      kdm.enableKwallet = true;
+      lightdm.enableKwallet = true;
+      sddm.enableKwallet = true;
+      slim.enableKwallet = true;
+    };
+
+    environment.systemPackages = with pkgs.libsForQt5; [
+      kwallet
+      kwallet-pam
+      kwalletmanager
+    ];
+
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.xfce = { enable = true; };
   };
