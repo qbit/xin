eb1d0f9c9495b9b5eff26e738b6c072cf6cf801c
Aaron Bieber <aaron@bolddaemon.com>
2023-01-04 10:09:41 -0700

    all: set more strict mac and kex for sshd

--- default.nix
+++ default.nix
@@ -176,6 +176,12 @@ in {
         enable = true;
         permitRootLogin = "prohibit-password";
         passwordAuthentication = false;
+        kexAlgorithms = [ "curve25519-sha256" "curve25519-sha256@libssh.org" ];
+        macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
       };
     };
   };
