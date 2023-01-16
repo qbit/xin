ecb8124fd248c4fe3f2410e3552e4793671ce694
Aaron Bieber <aaron@bolddaemon.com>
2023-01-16 06:43:06 -0700

    neovim: install mind, setup some lsp for lua and more telescope bindings

--- configs/neovim.lua
+++ configs/neovim.lua
@@ -17,18 +17,18 @@ cmd("hi! LineNr NONE");
 cmd("hi! Search cterm=NONE ctermbg=yellow");
 
 require("compe").setup {
-   enabled = true;
-   autocomplete = true;
-   source = {
-     path = true;
-     buffer = true;
-     calc = true;
-     nvim_lsp = true;
-     nvim_lua = true;
-     vsnip = true;
-     ultisnips = true;
-     luasnip = true;
-   };
+	enabled = true;
+	autocomplete = true;
+	source = {
+		path = true;
+		buffer = true;
+		calc = true;
+		nvim_lsp = true;
+		nvim_lua = true;
+		vsnip = true;
+		ultisnips = true;
+		luasnip = true;
+	};
 }
 
 local telescope = require('telescope')
@@ -36,37 +36,47 @@ telescope.load_extension('manix')
 telescope.load_extension('fzf')
 
 require("nvim-tree").setup({
-    renderer = {
-        icons = {
-	    show = false,
-	    webdev_colors = false,
-	    show = {
-               file = false,
-               folder = false,
-               folder_arrow = false,
-	       git = true,
-	    },
-        },
-    },
+	renderer = {
+		icons = {
+			--show = false,
+			webdev_colors = false,
+			show = {
+				file = false,
+				folder = false,
+				folder_arrow = false,
+				git = true,
+			},
+		},
+	},
 })
 
-cmd[[highlight NvimTreeOpenedFolderName guifg=default]]
-cmd[[highlight NvimTreeFolderName guifg=default]]
+require 'mind'.setup()
+
+cmd [[highlight NvimTreeOpenedFolderName guifg=default]]
+cmd [[highlight NvimTreeFolderName guifg=default]]
 
 local lspc = require("lspconfig")
 lspc.gopls.setup {};
 lspc.nil_ls.setup {};
+lspc.sumneko_lua.setup {};
 
 o.hlsearch = true;
 
-map('n', '<C-n>',     ':NvimTreeToggle<CR>',  {noremap = true})
-map('n', '<C-p>',     ':Files<CR>',           {noremap = true})
-map('n', '<C-m>',     ':Telescope manix<CR>', {noremap = true})
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true})
-map('n', '<leader>s', ':%s/\\s\\+$//e',       {noremap = true})
+map('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
+map('n', '<C-p>', ':Files<CR>', { noremap = true })
+map('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
+map('n', '<leader>s', ':%s/\\s\\+$//e', { noremap = true })
+map('n', '<leader>fm', ':Telescope manix<CR>', {})
 
-map('n', '<leader>1', ':GitGutterToggle<CR>', {noremap = true})
-map('n', '<leader>2', ':set list!<CR>',       {noremap = true})
-map('n', '<leader>3', ':set nu!<CR>',         {noremap = true})
-map('n', '<leader>4', ':set paste!<CR>',      {noremap = true})
+map('n', '<leader>1', ':GitGutterToggle<CR>', { noremap = true })
+map('n', '<leader>2', ':set list!<CR>', { noremap = true })
+map('n', '<leader>3', ':set nu!<CR>', { noremap = true })
+map('n', '<leader>4', ':set paste!<CR>', { noremap = true })
+vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
