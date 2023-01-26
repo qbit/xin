7303e9ce0d641ec5e5fa07e279034a0d8287f279
Aaron Bieber <aaron@bolddaemon.com>
2023-01-26 15:36:25 -0700

    pkgs/golink: bump to latest

--- pkgs/golink.nix
+++ pkgs/golink.nix
@@ -8,11 +8,11 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "tailscale";
     repo = pname;
-    rev = "3af59c51b849a19a01a47fe8bbad33dc16374201";
-    sha256 = "sha256-AAVX1G0ajN/G5IK4Xf7X8mrWj7LGblGCbd+x4BUZqrw=";
+    rev = "cada6f65af471470f85092a7152023e956ce0fb4";
+    sha256 = "sha256-YApJezFbihypIZx8UHqqhXQ/fw1Zz/XL6P6Z3gTFtrA=";
   };
 
-  vendorSha256 = "sha256-U3j5yiFhtYR0wvHD1U+DkYuFVt6NyEPlx7feLWfr3/Y=";
+  vendorSha256 = "sha256-0k+1G+ox9+NZI4GaHm2Ba2Q4Eybz20gTAPnGKkU5Iec=";
 
   proxyVendor = true;
 
