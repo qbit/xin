1b49c2ac8ffe7c535dd5a304f03ed47ab5e0022a
Aaron Bieber <aaron@bolddaemon.com>
2023-01-30 17:27:22 -0700

    pkgs/yarr: switch back to upstream now that manifest patch is in \o/

--- pkgs/yarr.nix
+++ pkgs/yarr.nix
@@ -3,24 +3,23 @@
 with lib;
 buildGoModule rec {
   pname = "yarr";
-  version = "2.3";
+  version = "2023-01-30";
 
   src = fetchFromGitHub {
     owner = "nkanaev";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-LW0crWdxS6zcY5rxR0F2FLDYy9Ph2ZKyB/5VFVss+tA=";
+    #rev = "v${version}";
+    rev = "c092842ee4a9621aff12d439f2fedd95058010fe";
+    sha256 = "sha256-VTEe+7x6DVXJFS+AEnfcUaag85zzK5Xve0zp1Polw7I=";
   };
 
-  vendorSha256 = "sha256-dseEvPu7VapyPzGKjqlFfXeaJdRF19YxCfZVn+ePYBc=";
+  vendorSha256 = null;
 
   ldflags = [ "-X main.Version=${version}" ];
 
   tags = [ "sqlite_foreign_keys" "release" ];
 
   proxyVendor = true;
-
-  patches = [ ./yarr_manifest.diff ];
 
   doCheck = false;
 
