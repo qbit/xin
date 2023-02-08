22c9956a0b336f3840b8ba63c9f94be02a535f1d
Aaron Bieber <aaron@bolddaemon.com>
2023-02-08 13:40:35 -0700

    pkgs: +ghexport

---
+++ pkgs/ghexport.nix
@@ -0,0 +1,28 @@
+{ pkgs, buildPythonPackage, setuptools-scm, fetchFromGitHub, PyGithub, pytz, ...
+}:
+
+buildPythonPackage rec {
+  pname = "ghexport";
+  version = "2022-08-28";
+
+  nativeBuildInputs = [ setuptools-scm ];
+  propagatedBuildInputs = [ PyGithub pytz ];
+
+  doCheck = true;
+
+  buildInputs = [ ];
+
+  preCheck = ''
+    export HOME=$(mktemp -d)
+  '';
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  src = fetchFromGitHub {
+    owner = "karlicoss";
+    repo = pname;
+    rev = "e7704dc5b984731a53e74cbcadcbc3dd9c3024cd";
+    hash = "sha256-m/iqeBvCXHlN7GsNW6A2AX1g+ZaH3W62+Ulcfgup0KQ=";
+  };
+}
+
