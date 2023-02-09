ca916153d9a521d4cf43cb72c2c3e4593e5e5d18
Aaron Bieber <aaron@bolddaemon.com>
2023-02-09 09:09:04 -0700

    overlays/tailscale: update to 1.36.1

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -3,14 +3,14 @@ let
     tailscale = super.callPackage "${super.path}/pkgs/servers/tailscale" {
       buildGoModule = args:
         super.buildGo119Module (args // rec {
-          version = "1.36.0";
+          version = "1.36.1";
           src = super.fetchFromGitHub {
             owner = "tailscale";
             repo = "tailscale";
             rev = "v${version}";
-            sha256 = "sha256-hNyEABs/GdfOx6vLTVBgbOzkbFvEDYZ0y1y0a0mIsfA=";
+            sha256 = "sha256-xTfMq8n9Io99qg/cc7SAWelcxXaWr21IQhsICeDCDNU=";
           };
-          vendorSha256 = "sha256-Jy3kjUA8qLhcw9XLw4Xo1zhD+IWZrDNM79TsbnKpx/g=";
+          vendorSha256 = "sha256-xdZlwv/2knOE7xaGeNHYNdztflhLLmirGzPOJpDvk3s=";
           ldflags = [
             "-X tailscale.com/version.Long=${version}"
             "-X tailscale.com/version.Short=${version}"
