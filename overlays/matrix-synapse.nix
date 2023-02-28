8b679c3a643fe1802771545fb3b79e78b49c0d68
Aaron Bieber <aaron@bolddaemon.com>
2023-02-28 15:04:06 -0700

    overlay/synapse: bump to 1.78.0

--- overlays/matrix-synapse.nix
+++ overlays/matrix-synapse.nix
@@ -1,20 +1,20 @@
 let
   matrix-synapse = self: super: {
     matrix-synapse = super.matrix-synapse.overrideAttrs (old: rec {
-      version = "1.77.0";
+      version = "1.78.0";
       pname = "matrix-synapse";
 
       src = super.fetchFromGitHub {
         owner = "matrix-org";
         repo = "synapse";
         rev = "v${version}";
-        hash = "sha256-//1BTiNH3n2eNjwOADb1OB7xp5QsH6arV5Pg3B7y3r0=";
+        hash = "sha256-UMP/JQ77qGfAQ+adLBLB8NFI2OiuwjILEbEecEDcK1A=";
       };
 
       cargoDeps = super.rustPlatform.fetchCargoTarball {
         inherit src;
         name = "${pname}-${version}";
-        hash = "sha256-B9Z+7VtbbX/S01aaMFHgXH60sg8Lmwku2XPRnpMpwjo=";
+        sha256 = "sha256-UTuMvTWfOlFlL+4qsCEfVljnkeylBKq0wd5FlAOYAFQ=";
       };
     });
   };
