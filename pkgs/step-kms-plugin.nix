5606218ff07967787267949780fd7428a96b5b9f
Aaron Bieber <aaron@bolddaemon.com>
2023-03-08 10:13:51 -0700

    pkgs/step-kms-plugin: sync with pr

--- pkgs/step-kms-plugin.nix
+++ pkgs/step-kms-plugin.nix
@@ -1,56 +1,37 @@
 { lib, buildGoModule, fetchFromGitHub, pkg-config, pcsclite, softhsm, opensc
-, writeScriptBin }:
+, yubihsm-shell, writeScriptBin }:
 
-let
-  getScriptName = "step-kms-module";
-  getSoftHSMsoPath = writeScriptBin getScriptName ''
-    #!/usr/bin/env sh
-    case $1 in
-    softhsm)
-      echo ${softhsm}/lib/softhsm/libsofthsm2.so
-      ;;
-    opensc)
-      echo ${opensc}/lib/opensc-pkcs11.so
-      ;;
-    *)
-      echo "usage: ${getScriptName} [softhsm|opensc]"
-    esac
-  '';
-
-in buildGoModule rec {
+buildGoModule rec {
   pname = "step-kms-plugin";
-  version = "0.6.1";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "smallstep";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-SvdV/eB+VWVMKPLptGWKPey4iUwkNXTyma+VBOzWwg8=";
+    hash = "sha256-5oMkR997ZbPpOqazpyxEvLKbak7THAu855FC6a/Tr+4=";
   };
+
+  vendorHash = "sha256-Zd2rZez5vP9uL5dolGHO8FR0ARoYP78amcakK/lKtdc=";
+
+  proxyVendor = true;
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ pcsclite softhsm opensc ];
+  buildInputs = [ opensc pcsclite softhsm yubihsm-shell ];
 
-  ldflags = [ "-w" "-s" "-X github.com/smallstep/step-kms-plugin/cmd.Version=${version}" ];
-
-  vendorHash = "sha256-Z1hMZGRLiLlrYlyV4GBL+zCPJv+i/EcZPI07RinpY2Q=";
-
-  postBuild = ''
-    mkdir -p $out/bin
-    ln -s ${getSoftHSMsoPath}/bin/${getScriptName} $out/bin/
-  '';
+  ldflags = [
+    "-w"
+    "-s"
+    "-X github.com/smallstep/step-kms-plugin/cmd.Version=${version}"
+  ];
 
   meta = with lib; {
     description =
       "step plugin to manage keys and certificates on cloud KMSs and HSMs";
-    longDescription = ''
-      An extra script (${getScriptName}) is included to return the path to 'libsofthsm2.so'.
-    '';
     homepage = "https://smallstep.com/cli/";
     license = licenses.asl20;
     maintainers = with maintainers; [ qbit ];
-    platforms = platforms.linux ++ platforms.darwin;
     mainProgram = "step-kms-plugin";
   };
 }
