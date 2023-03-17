eeba52571b794377209b110b5c9d80786084dc7c
Aaron Bieber <aaron@bolddaemon.com>
2023-03-17 13:31:41 -0600

    europa: add missing python dep

--- hosts/europa/default.nix
+++ hosts/europa/default.nix
@@ -291,7 +291,7 @@ in {
     (callPackage ../../pkgs/kobuddy.nix {
       inherit pkgs;
       inherit (pkgs.python39Packages)
-        buildPythonPackage fetchPypi setuptools-scm pytz banal sqlalchemy;
+        buildPythonPackage fetchPypi setuptools-scm pytz banal sqlalchemy alembic;
     })
     (callPackage ../../pkgs/gokrazy.nix { })
     (callPackage ../../pkgs/zutty.nix { })
