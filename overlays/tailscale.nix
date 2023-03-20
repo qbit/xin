8361376f28eda7ff72c4dfeecfdcda83402df1a6
Aaron Bieber <aaron@bolddaemon.com>
2023-03-20 08:58:28 -0600

    overlay/tailscale: set proper version

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -13,8 +13,8 @@ let
           vendorHash = "sha256-LIvaxSo+4LuHUk8DIZ27IaRQwaDnjW6Jwm5AEc/V95A=";
           vendorSha256 = "_unset";
           ldflags = [
-            "-X tailscale.com/version.Long=${version}"
-            "-X tailscale.com/version.Short=${version}"
+            "-X tailscale.com/version.longStamp=${version}"
+            "-X tailscale.com/version.shortStamp=${version}"
           ];
         });
     };
