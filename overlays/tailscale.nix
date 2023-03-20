1613c633c4af4d63f514c176938a2fe3fbc205ee
Aaron Bieber <aaron@bolddaemon.com>
2023-03-20 09:51:51 -0600

    overlay/tailscale: bump to latest

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -3,12 +3,12 @@ let
     tailscale = super.callPackage "${super.path}/pkgs/servers/tailscale" {
       buildGoModule = args:
         super.buildGo120Module (args // rec {
-          version = "1.38.0";
+          version = "1.38.1";
           src = super.fetchFromGitHub {
             owner = "tailscale";
             repo = "tailscale";
             rev = "v${version}";
-            hash = "sha256-eDEdllEvdZvP611x8MxlpR0QVz4N+bET407oNB3RQpQ=";
+            hash = "sha256-2VTYZhC/U32fzEJCxkCB35IqQBALKXQA23SvhY57lSU=";
           };
           vendorHash = "sha256-LIvaxSo+4LuHUk8DIZ27IaRQwaDnjW6Jwm5AEc/V95A=";
           vendorSha256 = "_unset";
