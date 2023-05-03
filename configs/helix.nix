5bc75bd346aad103a1878b39834211f9337e4e91
Aaron Bieber <aaron@bolddaemon.com>
2023-05-02 19:40:03 -0600

    configs/helix: add cursor-shape and some lsp configin from snk

--- configs/helix.nix
+++ configs/helix.nix
@@ -1,11 +1,17 @@
 { pkgs, ... }:
 
 let
-  tomlFmt = pkgs.formats.toml {};
+  tomlFmt = pkgs.formats.toml { };
   helixConfig = tomlFmt.generate "helix-config.toml" {
     theme = "acme";
     editor = {
       mouse = false;
+      cursor-shape = {
+        insert = "bar";
+        normal = "block";
+        select = "underline";
+      };
+      lsp = { auto-signature-help = false; };
     };
   };
   helixBin = "${pkgs.helix}/bin/hx";
