f42b7758c191321104bba3fdaa72e432d1324078
Aaron Bieber <aaron@bolddaemon.com>
2023-05-09 12:14:07 -0600

    fmt

--- pkgs/flake-warn.sh
+++ pkgs/flake-warn.sh
@@ -1,14 +1,13 @@
 #!@shell@ -e
 
-BOLD=$(tput bold)  
-NORMAL=$(tput sgr0)  
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 
-FLAKE_EPOCH=$(@nix@/bin/nix flake metadata --json | @jq@/bin/jq .lastModified)  
-NOW_EPOCH=$(@coreutils@/bin/date +"%s")  
+FLAKE_EPOCH=$(@nix@/bin/nix flake metadata --json | @jq@/bin/jq .lastModified)
+NOW_EPOCH=$(@coreutils@/bin/date +"%s")
 
-EPOCH_DIFF=$(($NOW_EPOCH - $FLAKE_EPOCH))  
+EPOCH_DIFF=$(($NOW_EPOCH - $FLAKE_EPOCH))
 
-if [ $EPOCH_DIFF -gt 60480 ]; then  
-	echo "${BOLD}WARNING: inputs haven't been updated in $(($EPOCH_DIFF / 86400)) days!${NORMAL}"  
-fi  
-
+if [ $EPOCH_DIFF -gt 60480 ]; then
+	echo "${BOLD}WARNING: inputs haven't been updated in $(($EPOCH_DIFF / 86400)) days!${NORMAL}"
+fi
