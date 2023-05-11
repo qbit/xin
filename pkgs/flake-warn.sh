b61677a3bd38630c1648df3ebef36c96982a43f9
Aaron Bieber <aaron@bolddaemon.com>
2023-05-11 05:56:50 -0600

    pkgs/flake-warn: add TODO

--- pkgs/flake-warn.sh
+++ pkgs/flake-warn.sh
@@ -3,6 +3,9 @@
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
+# TODO: Use the following for more accurate, to-the-input results:
+# nix flake metadata --json | jq -r '.locks.nodes[] | select(.original.repo == "nixpkgs" and .original.owner == "NixOS") | [ .original.ref, .locked.lastModified ] | join("^")'
+
 FLAKE_EPOCH=$(@nix@/bin/nix flake metadata --json | @jq@/bin/jq .lastModified)
 NOW_EPOCH=$(@coreutils@/bin/date +"%s")
 
