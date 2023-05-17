f469d95c66f2300980a698ddc13f0d5496bbd7e2
Aaron Bieber <aaron@bolddaemon.com>
2023-05-17 11:09:50 -0600

    overlays/jetbrains: update to 2023.1.2

--- overlays/jetbrains.nix
+++ overlays/jetbrains.nix
@@ -3,11 +3,11 @@ let
     # Override jetbrains idea-ultimate until the newer version is available
     jetbrains = super.jetbrains // {
       idea-ultimate = super.jetbrains.idea-ultimate.overrideAttrs (_: rec {
-        version = "2023.1";
+        version = "2023.1.2";
         src = super.fetchurl {
           url =
             "https://download-cdn.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-          sha256 = "sha256-MCnHUcNthv7wAh/s648wENN669Qq721q7Z47kgfC0qw=";
+          sha256 = "sha256-4aJgcOkb3Gp9JirtoxanKQjR/7uLUA8IZmW/zSneJJo=";
         };
       });
     };
