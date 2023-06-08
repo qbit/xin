012ff163a34f93bdd7db81deded76edf8235b82e
Aaron Bieber <aaron@bolddaemon.com>
2023-06-08 07:49:32 -0600

    overlays/matrix-synapse: 1.85.1 -> 1.85.2

--- overlays/matrix-synapse.nix
+++ overlays/matrix-synapse.nix
@@ -1,20 +1,20 @@
 let
   matrix-synapse = _: super: {
     matrix-synapse = super.matrix-synapse.overrideAttrs (_: rec {
-      version = "1.85.1";
+      version = "1.85.2";
       pname = "matrix-synapse";
 
       src = super.fetchFromGitHub {
         owner = "matrix-org";
         repo = "synapse";
         rev = "v${version}";
-        hash = "sha256-I/InjuTJOwYXw98qW7zT+fTnyLUo96xsVkFGSL+x+5k=";
+        hash = "sha256-pFafBsisBPfpDnFYWcimUuBgfFVPZzLna3yHeqIBAAE=";
       };
 
       cargoDeps = super.rustPlatform.fetchCargoTarball {
         inherit src;
         name = "${pname}-${version}";
-        sha256 = "sha256-KE56crjZDM1cJnVS7MvObIQ7NvH7+fUm1Mlb6HcT9+U=";
+        sha256 = "sha256-dnno+5Ma0YNYpmj3oZ5UG22uAanKwVT67BwQW+mHoFc=";
       };
     });
   };
