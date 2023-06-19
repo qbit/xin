d913afc294546bcfd50924b45290c4a084b61ddd
Aaron Bieber <aaron@bolddaemon.com>
2023-06-19 14:18:33 -0600

    fmt

--- hosts/europa/default.nix
+++ hosts/europa/default.nix
@@ -140,7 +140,8 @@ in {
       '';
       shellAliases = {
         "gh" = "op plugin run -- gh";
-        "nixpkgs-review" = "env GH_AUTH_TOKEN=$(op item get nixpkgs-review --field token) nixpkgs-review";
+        "nixpkgs-review" =
+          "env GH_AUTH_TOKEN=$(op item get nixpkgs-review --field token) nixpkgs-review";
         "godeps" =
           "go list -m -f '{{if not (or .Indirect .Main)}}{{.Path}}{{end}}' all";
         "mutt" = "neomutt -F /etc/neomuttrc";
