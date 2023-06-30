bee506ae2e41cd99e4902ece5ace55eaaebd0529
Aaron Bieber <aaron@bolddaemon.com>
2023-06-30 08:16:45 -0600

    overlays: sync

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,24 +1,17 @@
-{ isUnstable, xinlib, ... }:
-let
-  inherit (xinlib) prIsOpen;
-  #_1password-gui = prIsOpen.overlay 235900 (import ./1password-gui.nix);
-  #openssh = import ./openssh.nix;
-  #obsidian = prIsOpen.overlay 235408 (import ./obsidian.nix);
-  #tailscale = prIsOpen.overlay 239176 import ./tailscale.nix;
-  #jetbrains = prIsOpen 232308 (import ./jetbrains.nix);
-  tidal-hifi = prIsOpen.overlay 239732 (import ./tidal-hifi.nix);
-  matrix-synapse = prIsOpen.overlay 238845 (import ./matrix-synapse.nix);
-  nixd = prIsOpen.overlay 238779 (import ./nixd.nix);
-in {
-  nixpkgs.overlays = if isUnstable then [
-    #tailscale
-    tidal-hifi
-    nixd
-  ] else
-    [
-      matrix-synapse
-      #tailscale
-    ];
+{ isUnstable, ... }:
+#let
+#inherit (xinlib) prIsOpen;
+#_1password-gui = prIsOpen.overlay 235900 (import ./1password-gui.nix);
+#openssh = import ./openssh.nix;
+#obsidian = prIsOpen.overlay 235408 (import ./obsidian.nix);
+#tailscale = prIsOpen.overlay 239176 import ./tailscale.nix;
+#jetbrains = prIsOpen 232308 (import ./jetbrains.nix);
+#tidal-hifi = prIsOpen.overlay 239732 (import ./tidal-hifi.nix);
+#matrix-synapse = prIsOpen.overlay 238845 (import ./matrix-synapse.nix);
+#nixd = prIsOpen.overlay 238779 (import ./nixd.nix);
+#in {
+{
+  nixpkgs.overlays = if isUnstable then [ ] else [ ];
 }
 
 # Example Python dep overlay
