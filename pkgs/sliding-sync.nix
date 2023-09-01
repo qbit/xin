2d5b8a2c48c75a964b472833c52cd7e24b388b9c
Aaron Bieber <aaron@bolddaemon.com>
2023-09-01 07:24:18 -0600

    pkgs/sliding-sync: update to 0.99.8

--- pkgs/sliding-sync.nix
+++ pkgs/sliding-sync.nix
@@ -7,16 +7,16 @@
 with lib;
   buildGoModule rec {
     pname = "sliding-sync";
-    version = "0.99.6";
+    version = "0.99.8";
 
     src = fetchFromGitHub {
       owner = "matrix-org";
       repo = pname;
-      rev = "v${version}";
-      hash = "sha256-t0TlmoqXaKR5PrR0vlsLU84yBdXPXmE63n6p4sMvHhs=";
+      rev = "8e096656f58ffdc15ac5b08fc088eee1187a4a99";
+      hash = "sha256-4rYLHUlHbQ6KnxyXCCLqG4/zfXdZm4KZX1cOg5ITQPk=";
     };
 
-    vendorHash = "sha256-9bJ6B9/jq7q5oJGULRPoNVJiqoO+2E2QQKORy4rt6Xw=";
+    vendorHash = "sha256-JYSpjAgIvQFpYmOTifRXHVB6bSrukqSVhmAAmHylPbQ=";
 
     # Note: tests require a postgres install accessible to the current user
     doCheck = false;
