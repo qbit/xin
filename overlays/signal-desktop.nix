1e38084d210ec03bc92fc76e2bcc860aba0eab28
Aaron Bieber <aaron@bolddaemon.com>
2023-10-16 20:21:46 -0600

    overlays/signal-desktop: update to 6.34.1

--- overlays/signal-desktop.nix
+++ overlays/signal-desktop.nix
@@ -1,10 +1,10 @@
 let
   signal-desktop = _: super: {
     signal-desktop = super.signal-desktop.overrideAttrs (old: rec {
-      version = "6.34.0";
+      version = "6.34.1";
       src = super.fetchurl {
         url = "https://updates.signal.org/desktop/apt/pool/s/${old.pname}/${old.pname}_${version}_amd64.deb";
-        hash = "sha256-YsmXzorTYyydFcXME6GUxn/oaVosVfnCUjAurmlK+x8=";
+        hash = "sha256-1kffRXPQmtxIsLZVOgPXDnxUmY59q+1umy25cditRhw=";
       };
     });
   };
