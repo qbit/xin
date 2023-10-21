edd15f6e4501dab2cdf3ab0701c4eaab48576de6
Aaron Bieber <aaron@bolddaemon.com>
2023-10-21 15:26:46 -0600

    overlay/PLS: add missing FutureQueue dep

---
+++ overlays/PLS.nix
@@ -0,0 +1,36 @@
+let
+  perlPackages = _: super: {
+    PLS =
+      let
+        FutureQueue = super.buildPerlModule {
+          pname = "Future-Queue";
+          version = "0.51";
+          src = super.fetchurl {
+            url = "mirror://cpan/authors/id/P/PE/PEVANS/Future-Queue-0.51.tar.gz";
+            hash = "sha256-HVAcOpot3/x8YPlvpmlp1AyykuCSBM9t7NHCuLUAPNY=";
+          };
+          buildInputs = with super.perlPackages; [ Test2Suite ];
+          propagatedBuildInputs = with super.perlPackages; [ Future ];
+          meta = {
+            description = "A FIFO queue of values that uses L<Future>s";
+            license = with super.lib.licenses; [ artistic1 gpl1Plus ];
+          };
+        };
+      in
+      super.PLS.overrideAttrs (_: {
+        propagatedBuildInputs = with super.perlPackages; [
+          Future
+          FutureQueue
+          IOAsync
+          PPI
+          PPR
+          PathTiny
+          PerlCritic
+          PerlTidy
+          PodMarkdown
+          URI
+        ];
+      });
+  };
+in
+perlPackages
--- overlays/default.nix
+++ overlays/default.nix
@@ -15,11 +15,13 @@ let
   #rex = prIsOpen.overlay 0 (import ./rex.nix);
   signal-desktop = prIsOpen.overlay 260160 (import ./signal-desktop.nix);
   bruno = prIsOpen.overlay 260160 (import ./bruno.nix);
+  pls = prIsOpen.overlay 262524 (import ./PLS.nix);
 in
 {
   nixpkgs.overlays =
     if isUnstable
     then [
+      pls
       bruno
       signal-desktop
       #rex
