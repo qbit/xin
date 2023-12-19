50310e9ff8b617a854a9d49008f343a281056ae8
Aaron Bieber <aaron@bolddaemon.com>
2023-12-19 06:06:26 -0700

    configs/neovim: update obsidian-nvim to 2.3.1

--- configs/neovim.nix
+++ configs/neovim.nix
@@ -24,15 +24,15 @@ let
   };
   obsidian = vimBuildTool rec {
     pname = "obsidian-nvim";
-    version = "1.7.0"; # 1.8.0 has breaking changes
+    version = "2.3.1";
     src = pkgs.fetchFromGitHub {
       owner = "epwalsh";
       repo = "obsidian.nvim";
       rev = "v${version}";
-      sha256 = "sha256-pMYvtNEYoVFaWlj35F1rDlfJkNY4y4S62RNpHBNBgto=";
+      sha256 = "sha256-g9GFq5FMaCcJ6HbnhRgCmioLvaJ4SK6jSioDi5lXeP4=";
       fetchSubmodules = true;
     };
-    dependencies = with vimPlugins; [ nvim-cmp tabular ];
+    dependencies = with vimPlugins; [ nvim-cmp tabular plenary-nvim ];
   };
   neogen = vimBuildTool rec {
     pname = "neogen";
