95f5a5488ca8680c481befa71317a57d87a2e21e
Aaron Bieber <aaron@bolddaemon.com>
2023-12-19 05:49:15 -0700

    europa: use tlp stuff again

--- hosts/europa/default.nix
+++ hosts/europa/default.nix
@@ -180,6 +180,17 @@ in
   services.xinCA = { enable = false; };
 
   services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+        START_CHARGE_THRESH_BAT0 = 90;
+        STOP_CHARGE_THRESH_BAT0 = 97;
+        RUNTIME_PM_ON_BAT = "auto";
+      };
+    };
     logind = {
       lidSwitch = "suspend-then-hibernate";
       lidSwitchExternalPower = "lock";
