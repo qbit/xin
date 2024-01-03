bd568777f74bfc05858142c4c7d7d8672b818046
Aaron Bieber <aaron@bolddaemon.com>
2024-01-02 18:27:47 -0700

    all: switch to nix 2.17

--- system/nix-config.nix
+++ system/nix-config.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 let
   nixOptions = {
     gc = {
@@ -8,7 +8,7 @@ let
     };
 
     # Enable flakes
-    #package = pkgs.nixUnstable;
+    package = pkgs.nixVersions.nix_2_17;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
