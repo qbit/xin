fb6d7df0e770f1bfcb41b84a0814a298deac9d4e
Aaron Bieber <aaron@bolddaemon.com>
2024-01-20 20:24:23 -0700

    configs/alacritty: remove complications and make sure font exists

--- configs/alacritty.nix
+++ configs/alacritty.nix
@@ -17,30 +17,16 @@ let
   };
   settingsFormat = pkgs.formats.toml { };
   settingsFile = settingsFormat.generate "alacritty.toml" settings;
-  script = pkgs.writeScriptBin "alacritty-etc" ''
-    ${pkgs.alacritty}/bin/alacritty --config-file ${settingsFile}
-  '';
 in
 {
   config = {
-    nixpkgs.overlays = [
-      (self: super: {
-        alacritty = super.alacritty.overrideAttrs (old: {
-          postInstall = old.postInstall + ''
-            ${super.gnused}/bin/sed -i 's#^Exec=alacritty#Exec=alacritty --config-file ${settingsFile}#g' \
-              extra/linux/Alacritty.desktop
-            install -D extra/linux/Alacritty.desktop -t $out/share/applications/
-          '';
-        });
-      })
-    ];
     environment = {
-      systemPackages = [
-        script
-      ];
       etc = {
         "xdg/alacritty/alacritty.toml".text = builtins.readFile settingsFile;
       };
     };
+    fontSet = with pkgs; [
+      go-font
+    ];
   };
 }
