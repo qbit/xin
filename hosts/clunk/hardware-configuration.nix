43f4a296be6f76b5bf434e15e19bf086e533a494
Aaron Bieber <aaron@bolddaemon.com>
2024-01-31 18:23:14 -0700

    clunk: enable bluetooth

--- hosts/clunk/hardware-configuration.nix
+++ hosts/clunk/hardware-configuration.nix
@@ -27,5 +27,8 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware = {
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    bluetooth.enable = true;
+  };
 }
