c7ad822d73ac5dd0da9adc834a7494c5159e658f
Aaron Bieber <aaron@bolddaemon.com>
2024-02-19 13:57:48 -0700

    overlays/matrix-synapse: add patch to fix badges in elementx

--- overlays/matrix-synapse.nix
+++ overlays/matrix-synapse.nix
@@ -6,6 +6,13 @@ let
       version = "1.101.0";
       pname = "matrix-synapse";
 
+      patches = [
+        (super.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/element-hq/synapse/pull/16927.patch";
+          sha256 = "sha256-5Iy8oA/6XeOoe0d29ahFnEIn4zBcAOKtc/Jj5k3yb7c=";
+        })
+      ];
+
       src = super.fetchFromGitHub {
         owner = "element-hq";
         repo = "synapse";
