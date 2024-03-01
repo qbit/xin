fa6f998f4a0650f71d127eabfebb7e067492edbd
Aaron Bieber <aaron@bolddaemon.com>
2024-02-29 19:50:21 -0700

    bins/icb-irc-tunnel: try to log stuff

--- bins/icb-irc-tunnel.nix
+++ bins/icb-irc-tunnel.nix
@@ -5,8 +5,11 @@
   #!${pkgs.yash}/bin/yash
   ${pkgs.procps}/bin/pkill icbirc
 
-  ${icbirc}/bin/icbirc -l 127.0.0.1 -s localhost -p 6644
-  ${icbirc}/bin/icbirc -l 127.0.0.1 -s localhost -p 6645
+  # porters
+  ${icbirc}/bin/icbirc -d -l 127.0.0.1 -s localhost -p 6644 >/tmp/icbirc-porters &
+
+  # hackers
+  ${icbirc}/bin/icbirc -d -l 127.0.0.1 -s localhost -p 6645 >/tmp/icbirc-hackers &
 
   ${pkgs.openssh}/bin/ssh -NTL 7326:localhost:7326 \
     -oServerAliveInterval=60 \
