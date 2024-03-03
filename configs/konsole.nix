af9f489567d8e1d8811c800688c7ec08683203df
Aaron Bieber <aaron@bolddaemon.com>
2024-03-03 14:15:19 -0700

    configs/konsole: add working config / profile

--- configs/konsole.nix
+++ configs/konsole.nix
@@ -1,7 +1,32 @@
 { pkgs, ... }:
 let
+  profile = {
+    Appearance = {
+      AntiAliasFonts = true;
+      BoldIntense = true;
+      ColorScheme = "BlackOnLightYellow";
+      Font = "Go Mono,10,-1,5,50,0,0,0,0,0";
+      UseFontLineCharacters = false;
+    };
+    General = {
+      Name = "ARST";
+      Parent = "FALLBACK/";
+    };
+
+    "Interaction Options" = {
+      AutoCopySelectedText = true;
+      CopyTextAsHTML = false;
+    };
+
+    "Terminal Features" = {
+      BellMode = 3;
+    };
+  };
   settings = {
-    "MainWindow" = {
+    "Desktop Entry" = {
+      DefaultProfile = "${profile.General.Name}.profile";
+    };
+    MainWindow = {
       MenuBar = "Disabled";
       StatusBar = "Disabled";
       ToolBarsMovable = "Disabled";
@@ -9,11 +34,24 @@ let
   };
   settingsFormat = pkgs.formats.ini { };
   settingsFile = settingsFormat.generate "konsolerc" settings;
+  profileFile = settingsFormat.generate "${profile.General.Name}.profile" profile;
+
+  profilePkg = pkgs.stdenv.mkDerivation {
+    name = "konsole-profile";
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/share/konsole
+      cp ${profileFile} "$out/share/konsole/${profile.General.Name}.profile"
+    '';
+  };
 in
 {
   config = {
     environment = {
-      systemPackages = [ ];
+      systemPackages = [
+        profilePkg
+      ];
       etc = {
         "xdg/konsolerc".text = builtins.readFile settingsFile;
       };
