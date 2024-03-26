6918b7554103b7e3ff84bcc68256a6a58a94e4d5
Aaron Bieber <aaron@bolddaemon.com>
2024-03-26 07:48:01 -0600

    pkgs/gokrazy: bump to latest version

--- pkgs/gokrazy.nix
+++ pkgs/gokrazy.nix
@@ -6,16 +6,16 @@
 with lib;
 buildGoModule {
   pname = "gokrazy";
-  version = "0.0.0";
+  version = "2023-12-22";
 
   src = fetchFromGitHub {
     owner = "gokrazy";
     repo = "tools";
-    rev = "b89d9dc6e09742ea23492bb84021da70b2965bff";
-    sha256 = "sha256-1nWpLQMDvtV83HFvmrNdN31DVENq3HUqk/6+zuavoTU=";
+    rev = "80a59f115332a41206487afc6038beaaed48ce70";
+    sha256 = "sha256-YOo2VhidMbc5Cmc3YHx4hb/vlbnXeyrWWGxuSJPdX/o=";
   };
 
-  vendorHash = "sha256-d6je2aRHlgP4r/Yg55zezRMTul1p5aLEpxfLb3V6BFg=";
+  vendorHash = "sha256-BJTEP9n9oJcW2m5UAeExg3ydi+k9w4e+XLmxj/wGAl0=";
 
   proxyVendor = true;
 
