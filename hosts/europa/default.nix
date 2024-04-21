ea48a0b406bfe8383bd3e166ad008834a35cc726
Aaron Bieber <aaron@bolddaemon.com>
2024-04-20 18:26:13 -0600

    europa: enable memtest

--- hosts/europa/default.nix
+++ hosts/europa/default.nix
@@ -111,7 +111,10 @@ in
     binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
     initrd.systemd.enable = true;
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+      };
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot/efi";
