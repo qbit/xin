ed7e05d80235d40b6f140097dda5c399330ab5c5
Aaron Bieber <aaron@bolddaemon.com>
2024-05-12 14:34:00 -0600

    configs/nix: switch to lix

--- configs/nix.nix
+++ configs/nix.nix
@@ -1,21 +1,23 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, ... }:
 {
-  nix = {
-    package = inputs.unstable.legacyPackages.${pkgs.system}.nixVersions.nix_2_21;
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 10d";
-    };
+  nix =
+    let myPkgs = inputs.unstableSmall.legacyPackages.${pkgs.system};
+    in {
+      package = if lib.hasAttr "lix" myPkgs then myPkgs.lix else myPkgs.nixVersions.nix_2_21;
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 10d";
+      };
 
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-    settings.auto-optimise-store = true;
-    settings = {
-      sandbox = true;
-      trusted-users = [ "@wheel" ];
-      allowed-users = [ "root" "qbit" ];
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+      settings.auto-optimise-store = true;
+      settings = {
+        sandbox = true;
+        trusted-users = [ "@wheel" ];
+        allowed-users = [ "root" "qbit" ];
+      };
     };
-  };
 }
