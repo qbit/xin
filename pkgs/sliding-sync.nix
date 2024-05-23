f7bc337ecbc9128f1af54b8b93457fa848e15af6
Aaron Bieber <aaron@bolddaemon.com>
2024-05-23 13:34:04 -0600

    pkgs/sliding-sync: update to 0.99.18

--- pkgs/sliding-sync.nix
+++ pkgs/sliding-sync.nix
@@ -6,13 +6,13 @@
 with lib;
 buildGoModule rec {
   pname = "sliding-sync";
-  version = "0.99.17";
+  version = "0.99.18";
 
   src = fetchFromGitHub {
     owner = "matrix-org";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-tzhz2Jlhvn2blO5jdWNS++V28kNXmmg+a2BU7g5zTx0=";
+    hash = "sha256-zqqCgmzea25H1wcvgIb4hIV3maReL9tmNxvo9JsSlZk=";
   };
 
   vendorHash = "sha256-THjvc0TepIBFOTte7t63Dmadf3HMuZ9m0YzQMI5e5Pw=";
