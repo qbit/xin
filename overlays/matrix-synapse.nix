4bd030cd6d53a37b46988461a77253ef28c51029
Aaron Bieber <aaron@bolddaemon.com>
2024-07-03 15:10:27 -0600

    overlays/matrix-synapse: update to 1.110.0

--- overlays/matrix-synapse.nix
+++ overlays/matrix-synapse.nix
@@ -1,9 +1,10 @@
 let
-  hash = "sha256-AUaHgMKte1EIfI0EQm8YeQVtlXGTm+MZwq22WzYHGsE=";
-  sha256 = "sha256-KwRNn2Ypt87QRUTCsj00zsu6uQtP5MSuM6B2DemoFGs=";
+  version = "1.110.0";
+  hash = "sha256-DsDQgmHDU+iJ+00p1uch9Zj6lleDvdTQMy05hi8R9CM=";
+  sha256 = "sha256-J0JBp9pCP00Cjs6T4litjhY28mq0OJDBrRZVSQaS03w=";
   matrix-synapse-unwrapped = _: super: {
     matrix-synapse-unwrapped = super.matrix-synapse-unwrapped.overrideAttrs (_: rec {
-      version = "1.109.0";
+      inherit version;
       pname = "matrix-synapse";
 
       src = super.fetchFromGitHub {
