e4b960957fa6c3bb9547612ba6b55370fa815f49
Aaron Bieber <aaron@bolddaemon.com>
2024-07-08 07:15:04 -0600

    configs/ci: run update on monday and thursday

--- configs/ci.nix
+++ configs/ci.nix
@@ -12,7 +12,7 @@ let
       name = "xin-ci-update";
       user = "qbit";
       script = "cd ~/src/xin && ./bin/ci update";
-      startAt = "Sun 23:00";
+      startAt = "Mon,Thu 23:00";
       path = [ ];
     }
     {
