6b74ac52cf61d145d306b83cd62192ca79bd8beb
Aaron Bieber <aaron@bolddaemon.com>
2024-07-14 17:23:04 -0600

    tv: weee python youtubedl insecure

--- hosts/tv/default.nix
+++ hosts/tv/default.nix
@@ -26,6 +26,10 @@ in
 
   needsDeploy.enable = false;
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "python3.12-youtube-dl-2021.12.17"
+  ];
+
   boot = {
     loader = {
       systemd-boot.enable = true;
