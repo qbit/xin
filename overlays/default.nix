2bb67e3a189098fad5ca281cccf5ce2d93de87c1
Aaron Bieber <aaron@bolddaemon.com>
2024-07-19 09:13:56 -0600

    overlays: libressl tests work now

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,7 @@
 , ...
 }:
 let
-  inherit (xinlib) prIsOpen todo;
+  inherit (xinlib) prIsOpen;
   matrix-synapse-unwrapped = prIsOpen.overlay 0 (import ./matrix-synapse.nix);
   heisenbridge = prIsOpen.overlay 0 (import ./heisenbridge.nix);
 in
@@ -11,11 +11,6 @@ in
   nixpkgs.overlays = [
     heisenbridge
     matrix-synapse-unwrapped
-    (_: super: {
-      libressl = super.libressl.overrideAttrs (_: {
-        doCheck = todo "libressl tests disabled when building with musl" false;
-      });
-    })
   ] ++
   (if isUnstable
   then [
