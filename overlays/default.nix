e7e6daf8d9af6cd2f47db616059098cec01fb11a
Aaron Bieber <aaron@bolddaemon.com>
2024-07-24 15:11:51 -0600

    overlays/matrix-synapse: disable for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,13 +4,11 @@
 }:
 let
   inherit (xinlib) prIsOpen;
-  matrix-synapse-unwrapped = prIsOpen.overlay 0 (import ./matrix-synapse.nix);
   heisenbridge = prIsOpen.overlay 0 (import ./heisenbridge.nix);
 in
 {
   nixpkgs.overlays = [
     heisenbridge
-    matrix-synapse-unwrapped
   ] ++
   (if isUnstable
   then [
