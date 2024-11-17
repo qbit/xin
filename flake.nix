d8199c9c8404000672f3be2d8600e460efeeb889
Aaron Bieber <aaron@bolddaemon.com>
2024-11-17 06:16:07 -0700

    flake: update lix input

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     stable.url = "github:NixOS/nixpkgs/nixos-24.05-small";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
       inputs.nixpkgs.follows = "unstable";
     };
 
