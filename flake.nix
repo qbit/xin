34c205bdbaaa6dc714c16d33f64b0804ceead297
Aaron Bieber <aaron@bolddaemon.com>
2025-05-21 07:56:19 -0600

    Revert "flake: bump to 25.05 for stable"
    This reverts commit 0d8a619d982e0c5b2b0136510aed0dfbd1525623.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     unstable.url = "github:NixOS/nixpkgs";
     unstableSmall.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-    stable.url = "github:NixOS/nixpkgs/nixos-25.05-small";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.11-small";
 
     lix-module = {
       url = "git+https://git.lix.systems/lix-project/nixos-module";
