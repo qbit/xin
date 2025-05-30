fcad2ae7742adfbc49ceb0d0b5e31cd8529b3183
Aaron Bieber <aaron@bolddaemon.com>
2025-05-30 08:03:22 -0600

    box,h: remove loki stuff

--- hosts/box/default.nix
+++ hosts/box/default.nix
@@ -515,15 +515,6 @@ in
             access = "proxy";
             url = "http://127.0.0.1:${toString config.services.prometheus.port}";
           }
-          {
-            name = "Loki";
-            type = "loki";
-            access = "proxy";
-            url = "http://127.0.0.1:${
-              toString
-              config.services.loki.configuration.server.http_listen_port
-            }";
-          }
         ];
       };
     };
--- hosts/h/default.nix
+++ hosts/h/default.nix
@@ -600,35 +600,6 @@ in
         landing-page-user = "qbit";
       };
     };
-    promtail = {
-      enable = true;
-      configuration = {
-        server = {
-          http_listen_port = 3031;
-          grpc_listen_port = 0;
-        };
-        positions = { filename = "/tmp/positions.yaml"; };
-        clients = [{ url = "http://box.otter-alligator.ts.net:3030/loki/api/v1/push"; }];
-        scrape_configs = [
-          {
-            job_name = "journal";
-            journal = {
-              max_age = "12h";
-              labels = {
-                job = "systemd-journal";
-                host = "box";
-              };
-            };
-            relabel_configs = [
-              {
-                source_labels = [ "__journal__systemd_unit" ];
-                target_label = "unit";
-              }
-            ];
-          }
-        ];
-      };
-    };
     prometheus = {
       enable = true;
       port = 9001;
