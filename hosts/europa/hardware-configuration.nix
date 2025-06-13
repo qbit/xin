608191d84de4ccf2dec84126ed10a7219c1625b8
Aaron Bieber <aaron@bolddaemon.com>
2025-06-13 07:50:24 -0600

    europa: bump cache on davfs

--- hosts/europa/hardware-configuration.nix
+++ hosts/europa/hardware-configuration.nix
@@ -39,7 +39,9 @@
     davUser = "qbit";
     settings = {
       globalSection = {
+        cache_size = 1000;
         ask_auth = false;
+        minimize_mem = true;
       };
       sections = {
         "/run/media/qbit/TailDrive" = {
@@ -52,55 +54,64 @@
     };
   };
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/0b946ca0-f0cb-4e54-bc73-d2afe6b328d2";
-      fsType = "ext4";
-    };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/3D38-3AEC";
-      fsType = "vfat";
-      options = [
-        "fmask=0022"
-        "dmask=0022"
-      ];
-    };
+  fileSystems =
+    let
+      qbitUid = toString config.users.users.qbit.uid;
+      qbitGid = toString config.users.groups.wheel.gid;
+    in
+    {
+      "/" = {
+        device = "/dev/disk/by-uuid/0b946ca0-f0cb-4e54-bc73-d2afe6b328d2";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-uuid/3D38-3AEC";
+        fsType = "vfat";
+        options = [
+          "fmask=0022"
+          "dmask=0022"
+        ];
+      };
 
-    "/run/media/qbit/backup" = {
-      device = "/dev/disk/by-uuid/6e71eeea-6437-46f4-88d0-126c92af42ef";
-      fsType = "ext4";
-      neededForBoot = false;
-    };
+      "/run/media/qbit/backup" = {
+        device = "/dev/disk/by-uuid/6e71eeea-6437-46f4-88d0-126c92af42ef";
+        fsType = "ext4";
+        neededForBoot = false;
+      };
 
-    "/run/media/qbit/keestore" = {
-      device = "http://100.100.100.100:8080/tapenet.org/box/keestore";
-      fsType = "davfs";
-      options = [
-        "_netdev"
-        "x-systemd.automount"
-        "reconnect"
-        "rw"
-        "user"
-        "uid=1000"
-        "gid=1000"
-      ];
-      neededForBoot = false;
+      "/run/media/qbit/keestore" = {
+        device = "http://100.100.100.100:8080/tapenet.org/box/keestore";
+        fsType = "davfs";
+        options = [
+          "_netdev"
+          "x-systemd.automount"
+          "x-systemd.device-timeout=5s"
+          "x-systemd.mount-timeout=5s"
+          "reconnect"
+          "rw"
+          "user"
+          "uid=${qbitUid}"
+          "gid=${qbitGid}"
+        ];
+        neededForBoot = false;
+      };
+      "/run/media/qbit/TailDrive" = {
+        device = "http://100.100.100.100:8080/tapenet.org/box/media";
+        fsType = "davfs";
+        options = [
+          "_netdev"
+          "x-systemd.automount"
+          "x-systemd.device-timeout=5s"
+          "x-systemd.mount-timeout=5s"
+          "reconnect"
+          "rw"
+          "user"
+          "uid=${qbitUid}"
+          "gid=${qbitGid}"
+        ];
+        neededForBoot = false;
+      };
     };
-    "/run/media/qbit/TailDrive" = {
-      device = "http://100.100.100.100:8080/tapenet.org/box/media";
-      fsType = "davfs";
-      options = [
-        "_netdev"
-        "x-systemd.automount"
-        "reconnect"
-        "rw"
-        "user"
-        "uid=1000"
-        "gid=1000"
-      ];
-      neededForBoot = false;
-    };
-  };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/1c2bb5e0-7ca8-4943-8e0f-527497ce2d61"; } ];
 
