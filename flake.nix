e78dfc3366cc3221fe398b328865786e4dd67ab1
Aaron Bieber <aaron@bolddaemon.com>
2025-06-30 11:56:24 -0600

    flake: update lix to 2.93.2-1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     stable.url = "github:NixOS/nixpkgs/nixos-25.05-small";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
       inputs.nixpkgs.follows = "unstableSmall";
     };
 
