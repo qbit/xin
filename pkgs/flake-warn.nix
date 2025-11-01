043356cccdebf129f3b468f3ea34e59838032c75
Aaron Bieber <aaron@bolddaemon.com>
2025-11-01 09:44:22 -0600

    pkgs/flake-warn: migrate to new shit

--- pkgs/flake-warn.nix
+++ pkgs/flake-warn.nix
@@ -1,10 +1,10 @@
 {
   stdenv,
   lib,
-  substituteAll,
   jq,
   nix,
   coreutils,
+  writeTextFile,
   ...
 }:
 stdenv.mkDerivation rec {
@@ -15,13 +15,29 @@ stdenv.mkDerivation rec {
     install -Dm755 $script $out/bin/${pname}
   '';
 
-  script = substituteAll {
-    src = ./flake-warn.sh;
-    isExecutable = true;
-    inherit jq nix coreutils;
-    inherit (stdenv) shell;
-  };
+  script = writeTextFile {
+    name = pname;
+    text = ''
+      #!/bin/sh
 
+      BOLD=$(tput bold)
+      NORMAL=$(tput sgr0)
+
+      # TODO: Use the following for more accurate, to-the-input results:
+      # nix flake metadata --json | jq -r '.locks.nodes[] | select(.original.repo == "nixpkgs" and .original.owner == "NixOS") | [ .original.ref, .locked.lastModified ] | join("^")'
+
+      FLAKE_EPOCH=$(${nix}/bin/nix flake metadata --json | ${jq}/bin/jq .lastModified)
+      NOW_EPOCH=$(${coreutils}/bin/date +"%s")
+
+      EPOCH_DIFF=$((NOW_EPOCH - FLAKE_EPOCH))
+
+      if [ $EPOCH_DIFF -gt $((60480 * 5)) ]; then
+      	echo
+      	echo "$BOLDWARNING: inputs haven't been updated in $((EPOCH_DIFF / 86400)) days!$NORMAL"
+      	echo
+      fi
+    '';
+  };
   meta = {
     description = "script to warn when flake inputs are out of date";
     homepage = "https://codeberg.org/qbit/xin";
