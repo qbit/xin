68cb0ca44fce8381443f550d5f0c4eb44d3cdd5c
Aaron Bieber <aaron@bolddaemon.com>
2025-11-25 07:15:16 -0700

    configs/firefox: stop forcing 1password

--- configs/firefox.nix
+++ configs/firefox.nix
@@ -25,7 +25,7 @@ let
     };
     ExtensionSettings = {
       "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-        "installation_mode" = "force_installed";
+        "installation_mode" = "normal_installed";
         "install_url" =
           "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
       };
