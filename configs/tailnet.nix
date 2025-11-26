7e1260ece9ad385ed61c79625d509247249fc14b
Aaron Bieber <aaron@bolddaemon.com>
2025-11-26 11:59:29 -0700

    configs/tailnet: update config and clean up a bunch

--- configs/tailnet.nix
+++ configs/tailnet.nix
@@ -62,16 +62,14 @@ let
           homeassistant = "100.68.29.6";
           immich = "100.90.44.82";
           box = "100.115.16.150";
-          console = "100.83.166.33";
           europa = "100.64.26.122";
-          faf = "100.80.94.131";
+          hole = "100.104.223.100";
           gitle = "100.111.162.87";
-          graphy = "100.123.184.55";
+          graphy = "100.76.188.108";
           h = "100.83.77.133";
           il = "100.86.182.99";
           namish = "100.86.184.141";
-          nbc = "100.74.8.55";
-          ollama = "100.121.227.121";
+          nbc = "100.74.8.17";
           plq = "100.90.214.142";
           pwntie = "100.84.170.57";
           rimgo = "100.121.77.91";
@@ -96,117 +94,137 @@ let
           "tag:work" = [ "qbit@tapenet.org" ];
           "tag:dns-server" = [ "qbit@tapenet.org" ];
           "tag:openbsd" = [ "qbit@tapenet.org" ];
+          "tag:kdeconnect" = [ "qbit@tapenet.org" ];
         };
 
-        acls = [
-          {
-            action = "accept";
-            src = [
-              "pwntie"
-              "tag:mobile"
-              "tag:laptop"
-            ];
-            dst = [ "h:6697" ];
-          }
-          {
-            action = "accept";
-            src = [
-              "box"
-              "homeassistant"
-            ];
-            dst = [ "printy:443" ];
-          }
-          {
-            # Allow laptops and mobile devices to ssh to everything
-            action = "accept";
-            src = [
-              "tag:mobile"
-              "tag:laptop"
-            ];
-            dst = [ "*:*" ];
-          }
-          {
-            action = "accept";
-            src = [
-              "tag:internal-server"
-              "tag:external-server"
-              "tag:work"
-              "tag:laptop"
-            ];
-            dst = [ "nbc:443" ];
-          }
-          {
-            action = "accept";
-            src = [
-              "tag:untrusted"
-              "tag:internal-server"
-            ];
-            dst = [ "tag:ro-service:443" ];
-          }
-          {
-            action = "accept";
-            src = [ "tag:work" ];
-            dst = [
-              "console:2222"
-              "startpage:443"
-              "rimgo:443"
-            ];
-          }
-          {
-            action = "accept";
-            src = [
-              "tag:openbsd"
-              "tag:internal-server"
-            ];
-            dst = [ "box:443" ];
-          }
-          {
-            # prometheus
-            action = "accept";
-            src = [
-              "box"
-              "homeassistant"
-            ];
-            dst = [
-              "h:9002"
-              "pwntie:9002"
-              "box:9001"
-            ];
-          }
-          {
-            # DNS
-            action = "accept";
-            src = [ "*" ];
-            dst = [ "faf:53" ];
-            proto = "udp";
-          }
-          {
-            # ollama
-            action = "accept";
-            src = [
-              "europa"
-              "h"
-              "tag:work"
-            ];
-            dst = [ "ollama:443" ];
-            proto = "tcp";
-          }
-          {
-            action = "accept";
-            src = [ "h" ];
-            dst = [
-              "ollama:443"
-              "notify:443"
-            ];
-            proto = "tcp";
-          }
-        ];
+        acls =
+          let
+            mkRange =
+              start: stop: entry:
+              builtins.map (a: "${entry}:${toString a}") (lib.range start stop);
+            mkSet = list: entry: builtins.map (a: "${entry}:${toString a}") list;
+          in
+          [
+            {
+              # Allow everything to get to DNS
+              action = "accept";
+              src = [ "*" ];
+              dst = [ "tag:dns-server:53" ];
+            }
+            {
+              # Allow everything to get to the nix-binary-cache
+              action = "accept";
+              src = [
+                "*"
+              ];
+              dst = [ "nbc:443" ];
+              proto = "tcp";
+            }
+            {
+              # Allow laptops and mobile devices to ssh to everything and manage DNS
+              action = "accept";
+              src = [
+                "tag:mobile"
+                "tag:laptop"
+              ];
+              dst = [
+                "*:22"
+                "tag:dns-server:443"
+                "tag:dns-server:80"
+              ];
+            }
+            {
+              # IRC
+              action = "accept";
+              src = [
+                "pwntie"
+                "tag:mobile"
+                "tag:laptop"
+              ];
+              dst = [ "h:6697" ];
+            }
+            {
+              # HomeAssistant talks to octoprint
+              action = "accept";
+              src = [
+                "homeassistant"
+              ];
+              dst = [ "printy:443" ];
+            }
+            {
+              # KDEConnect
+              action = "accept";
+              src = [
+                "tag:kdeconnect"
+              ];
+              dst = (mkRange 1714 1764 "tag:kdeconnect");
+            }
+            {
+              # Mosh
+              action = "accept";
+              src = [
+                "tag:laptop"
+                "tag:mobile"
+              ];
+              dst = (mkRange 60000 61000 "h") ++ (mkRange 60000 61000 "pwntie");
+              proto = "udp";
+            }
+            {
+              action = "accept";
+              src = [
+                # OpenBSD connecting to the nginx caching proxy
+                "tag:openbsd"
+                "tag:internal-server"
+
+                # Other things connecting to 443 for nginx reverse proxy
+                "tag:laptop"
+                "tag:mobile"
+              ];
+              dst = [ "box:443" ];
+            }
+            {
+              # Prometheus
+              action = "accept";
+              src = [
+                "box"
+              ];
+              dst = [
+                "h:9002"
+                "pwntie:9002"
+                "box:9001"
+              ];
+            }
+            {
+              # Notifications from specific servers
+              action = "accept";
+              src = [ "h" ];
+              dst = [
+                "notify:443"
+              ];
+              proto = "tcp";
+            }
+            {
+              # Let various devices connect to web services and ssh ports
+              action = "accept";
+              src = [
+                "tag:laptop"
+                "tag:mobile"
+                "tag:internal-server"
+              ];
+              dst = [ "tag:untrusted:2222" ] ++ (mkSet [ 80 443 22 2222 ] "tag:ro-service");
+
+              proto = "tcp";
+            }
+          ];
 
         tests = [
           {
+            # h can get to notification stuff, hole for dns and nbc for updates
             src = "h";
             allow = [
               "notify:443"
+              "nbc:443"
+              "hole:53"
             ];
           }
           {
@@ -219,67 +237,41 @@ let
             ];
           }
           {
+            # Prevent external server from getting to sensitive things
             src = "tag:external-server";
             deny = [ "tag:laptop:22" ];
           }
           {
+            # laptop should be able to access various services
             src = "tag:laptop";
             allow = [
+              "gitle:22"
+              "pwntie:60000"
+              "tag:external-server:22"
               "tag:ro-service:443"
               "tag:ro-service:80"
-              "tag:external-server:22"
-            ];
-          }
-          {
-            src = "tag:laptop";
-            allow = [ "qbit@tapenet.org:22" ];
-          }
-          {
-            src = "tag:untrusted";
-            deny = [ "tag:laptop:22" ];
-            allow = [ "tag:ro-service:443" ];
-          }
-          {
-            src = "tag:laptop";
-            allow = [
               "tag:untrusted:22"
               "tag:untrusted:2222"
               "tag:work:22"
             ];
           }
           {
-            src = "tag:work";
-            deny = [ "tag:laptop:22" ];
-          }
-
-          # Gitle shouldn't be able to access things, but things should access it
-          {
-            src = "gitle";
-            deny = [ "tag:laptop:22" ];
-          }
-          {
+            # laptops can access ssh on anything
             src = "tag:laptop";
-            allow = [ "gitle:22" ];
+            allow = [ "qbit@tapenet.org:22" ];
           }
           {
-            src = "tag:laptop";
-            allow = [ "faf:53" ];
-          }
-          {
-            src = "tag:internal-server";
-            allow = [
-              "nbc:443"
+            # untrusted can't access 22 or 443 on ro-service
+            src = "tag:untrusted";
+            deny = [
+              "tag:laptop:22"
               "tag:ro-service:443"
             ];
           }
           {
-            "src" = "tag:laptop";
-            allow = [ "h:8967" ];
-          }
-          {
-            src = "h";
-            proto = "udp";
-            allow = [ "faf:53" ];
+            # prevent work machine from coming back in
+            src = "tag:work";
+            deny = [ "tag:laptop:22" ];
           }
           {
             src = "tag:openbsd";
@@ -295,11 +287,6 @@ let
             src = "sputnik";
             proto = "udp";
             allow = [ "europa:1714" ];
-          }
-          {
-            src = "europa";
-            proto = "tcp";
-            allow = [ "ollama:443" ];
           }
           {
             src = "lroom";
